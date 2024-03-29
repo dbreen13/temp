7164df59a0244565e4e6212cea92b88f6efda141
dbreen13 <demibreen@gmail.com>
2024-03-29 18:34:54 +0100

    jajaja

--- piektest.sh
+++ piektest.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 #train limits
-#for i in {1..5};do
-#    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tucker-r0.9-63.yml";
-#    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i;
-#done; 
+CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-63.yml";
+python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=6 --epochs=1;
 
-#for i in {1..5}; do
-#    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-63.yml";
-#    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i ;
-#done;
 
-#for i in {1..2};do
-#    python train.py main --config-path /home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tt-r0.9-63.yml --data_workers=4 --index=$i;  
-#done;
+CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-63.yml";
+python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=6 --epochs=1;
 
-#for i in {1..2};do
-#    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-63.60.yml";
-#    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i;
-#done;
+for i in {1..5};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tucker-r0.9-63.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i;
+done; 
 
-#for i in {1..2};do
-#    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-63.60.44.41.yml";
-#    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i;
-#done;
+for i in {1..5}; do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-63.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i ;
+done;
+
+for i in {1..2};do
+    python train.py main --config-path /home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tt-r0.9-63.yml --data_workers=4 --index=$i;  
+done;
+
+for i in {1..2};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-63.60.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i --epochs=1;
+done;
+
+for i in {1..2};do
+   CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-63.60.44.41.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i --epochs=1;
+done;
 
 #train baseline
 for i in {1..2};do
@@ -36,6 +43,11 @@ for i in {1..2};do
 done;
 
 for i in {1..2};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/garipov/fmnist/train_garipov.yml";
+   CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/garipov/fmnist/train_garipov.yml";
     python train.py main --config-path "$CONFIG_PATH" --data_workers=4 --epochs=10 --index=$i;
 done;
+
+
+
+
+
